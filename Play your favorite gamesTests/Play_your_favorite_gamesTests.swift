35e48490577791574b237635668a49f8ae0e04ef
komiselena <komiselen@yandex.ru>
2025-05-29 12:01:07 +0300

    Initial Commit

---
+++ Play your favorite gamesTests/Play_your_favorite_gamesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Play_your_favorite_gamesTests.swift
+//  Play your favorite gamesTests
+//
+//  Created by Mac on 29.05.2025.
+//
+
+import Testing
+@testable import Play_your_favorite_games
+
+struct Play_your_favorite_gamesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
