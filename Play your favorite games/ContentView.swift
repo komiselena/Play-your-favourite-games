25590bc30f88d56b0753db1ab4e5bffa64d26a2e
komiselena <komiselen@yandex.ru>
2025-06-03 20:07:01 +0300

    ipad fixes

--- Play your favorite games/ContentView.swift
+++ Play your favorite games/ContentView.swift
@@ -56,10 +56,11 @@ struct ContentView: View {
                     .padding(.top, isiPad ? g.size.height * 0.25 : g.size.height * 0.3)
                     .frame(width: isiPad ? g.size.width * 0.18 : g.size.width * 0.2,
                            height: isiPad ? g.size.height * 0.45 : g.size.height * 0.5)
+                    .padding(.leading, isiPad ? g.size.width * 0.15 : 0)
 
                     // Уменьшаем спейсер для iPad
                     Spacer()
-                        .frame(width: isiPad ? g.size.width * 0.5 : g.size.width * 0.6,
+                        .frame(width: isiPad ? g.size.width * 0.6 : g.size.width * 0.6,
                                height: isiPad ? g.size.height * 0.6 : g.size.height * 0.7)
 
                     Image("UiBar")
@@ -67,9 +68,9 @@ struct ContentView: View {
                         .scaledToFit()
                         .frame(width: isiPad ? g.size.width * 0.18 : g.size.width * 0.2,
                                height: isiPad ? g.size.height * 0.6 : g.size.height * 0.7)
-                        .padding(.trailing, isiPad ? 0 : g.size.width * 0.2)
+                        .padding(.trailing, isiPad ? g.size.width * 0.1 : g.size.width * 0.2)
                 }
-                .frame(width: g.size.width * 0.9, height: g.size.height * 0.9)
+                .frame(width: g.size.width * 0.95, height: g.size.height * 0.9)
             }
             .overlay(
                 Button {
